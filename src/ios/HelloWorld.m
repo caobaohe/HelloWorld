0f5e0e284191657a9729fe500cf0fb49923d2a57
caobaohe <13647008140@163.com>
2017-08-01 12:56:10 +0800

    ok
    ok

--- src/ios/HelloWorld.m
+++ src/ios/HelloWorld.m
@@ -22,9 +22,6 @@
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 
-	//
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=General"]];
-
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
